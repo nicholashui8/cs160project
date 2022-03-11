2f9e781cc6c16b84928a1145b3353ecd1039fc38
nicholashui <nicholas.hui@sjsu.edu>
2022-03-10 18:52:27 -0800

    finished setting up database

--- backend/build/index.js
+++ backend/build/index.js
@@ -1,9 +1,16 @@
 "use strict";
 const express = require('express');
 const app = express();
+const connectDB = require('./db/connectDB');
+const Student = require('./db/models/Student');
 app.get('/', (req, res) => {
     res.send('Well done!');
 });
 app.listen(8000, () => {
     console.log('The application is listening on port 8000! Hello!');
 });
+//connects to database
+connectDB();
+//creates a new document in the 'student' collection
+Student.create({ name: 'Nic', studentID: 324 });
+module.exports = {};
